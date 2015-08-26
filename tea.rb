f4ce59e8f41b53de22c8ce6697b818bba4b69369
Leigh Halliday <leighhalliday@gmail.com>
2015-08-25 21:05:28 -0400

    More llama pics

--- tea.rb
+++ tea.rb
@@ -1,41 +1,41 @@
 class Tea
-  attr_accessor :name, :cost, :caffiene, :steep
+  attr_accessor :name, :cost, :caffeine, :steep
 
   def initialize(options)
     @name = options[:name]
     @cost = options[:cost]
-    @caffiene = options[:caffiene]
+    @caffeine = options[:caffeine]
     @steep = options[:steep]
   end
 
-  def is_caffienated?
-    caffiene > 0
+  def is_caffeinated?
+    caffeine > 0
   end
 end
 
 teas = []
-teas << Tea.new(name: "Silver Needle", cost: 0.64, caffiene: 30, steep: 180)
-teas << Tea.new(name: "Ceylon",        cost: 0.30, caffiene: 70, steep: 240)
-teas << Tea.new(name: "Peppermint",    cost: 0.20, caffiene: 0,  steep: 300)
-teas << Tea.new(name: "Sencha",        cost: 0.70, caffiene: 30, steep: 90)
+teas << Tea.new(name: "Silver Needle", cost: 0.64, caffeine: 30, steep: 180)
+teas << Tea.new(name: "Ceylon",        cost: 0.30, caffeine: 70, steep: 240)
+teas << Tea.new(name: "Peppermint",    cost: 0.20, caffeine: 0,  steep: 300)
+teas << Tea.new(name: "Sencha",        cost: 0.70, caffeine: 30, steep: 90)
 
 # Count
-puts "Count"
+puts "\nCount"
 
 # Any?
-puts "Any?"
+puts "\nAny?"
 
 # All?
-puts "All?"
+puts "\nAll?"
 
 # Select
-puts "Select"
+puts "\nSelect"
 
 # Find
-puts "Find"
+puts "\nFind"
 
-# Min
-puts "Min"
+# Min By
+puts "\nMin By"
 
 # Map
-puts "Map"
+puts "\nMap"
--- tea_finished.rb
+++ tea_finished.rb
@@ -1,50 +1,49 @@
 class Tea
-  attr_accessor :name, :cost, :caffiene, :steep
+  attr_accessor :name, :cost, :caffeine, :steep
 
   def initialize(options)
     @name = options[:name]
     @cost = options[:cost]
-    @caffiene = options[:caffiene]
+    @caffeine = options[:caffeine]
     @steep = options[:steep]
   end
 
-  def is_caffienated?
-    caffiene > 0
+  def is_caffeinated?
+    caffeine > 0
   end
 end
 
 teas = []
-teas << Tea.new(name: "Silver Needle", cost: 0.64, caffiene: 30, steep: 180)
-teas << Tea.new(name: "Ceylon",        cost: 0.30, caffiene: 70, steep: 240)
-teas << Tea.new(name: "Peppermint",    cost: 0.20, caffiene: 0,  steep: 300)
-teas << Tea.new(name: "Sencha",        cost: 0.70, caffiene: 30, steep: 90)
+teas << Tea.new(name: "Silver Needle", cost: 0.64, caffeine: 30, steep: 180)
+teas << Tea.new(name: "Ceylon",        cost: 0.30, caffeine: 70, steep: 240)
+teas << Tea.new(name: "Peppermint",    cost: 0.20, caffeine: 0,  steep: 300)
+teas << Tea.new(name: "Sencha",        cost: 0.70, caffeine: 30, steep: 90)
 
 # Count
-puts "Count"
+puts "\nCount"
 puts teas.count
 
 # Any?
-puts "Any?"
-puts teas.any?{ |tea| tea.name == "Sencha" }
-puts teas.any?{ |tea| tea.name == "Rooibos" }
+puts "\nAny?"
+puts teas.any?{ |tea| tea.cost >= 1.50 }
 
 # All?
-puts "All?"
-puts teas.all?(&:is_caffienated?)
+puts "\nAll?"
+puts teas.all?{ |tea| tea.is_caffeinated? }
+puts teas.all?(&:is_caffeinated?)
 
 # Select
-puts "Select"
-p teas.select(&:is_caffienated?)
-p teas.select{ |tea| tea.cost >= 0.50 }
+puts "\nSelect"
+puts teas.select{ |tea| tea.steep <= 240 }.inspect
 
 # Find
-puts "Find"
-p teas.find{ |tea| tea.name == "Sencha" }
+puts "\nFind"
+puts teas.find{ |tea| tea.name == "Peppermint" }.inspect
 
-# Min
-puts "Min"
-p teas.min{ |tea| tea.steep }
+# Min By
+puts "\nMin By"
+puts teas.min_by(&:cost).inspect
 
 # Map
-puts "Map"
-p teas.map{ |tea| "#{tea.name} - #{tea.cost}" }
+puts "\nMap"
+puts teas.map{ |tea| "#{tea.name} - #{tea.cost}" }.inspect
